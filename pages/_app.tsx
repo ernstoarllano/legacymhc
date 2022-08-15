253e4756eef1f015f87fffe6cdfd7f8d39a05cc9
Ernesto Arellano <ernestoarellano@icloud.com>
2022-08-14 22:33:58 -0700

    ditch google analytics for plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@fontsource/oxygen/300.css'
 import '@fontsource/oxygen/400.css'
 import '@fontsource/oxygen/700.css'
+import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="legacymhc.vercel.app">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
-import Script from 'next/script'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,21 +16,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-G3KLN4C3CN"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-G3KLN4C3CN');
-            `}
-          </Script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
